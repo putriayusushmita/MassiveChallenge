deb2c9a320f035557878485db2de306dc30d8546
putriayusushmita <putriayusushmita6@gmail.com>
2021-12-28 10:30:24 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/JambuAirActivity2.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class JambuAirActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jambubiji2);
+    }
+}
